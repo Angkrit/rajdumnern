5dbc34236fd9dae1fe58cb9e998794e5270a4089
Angkrit <angkrit@live.com>
2014-05-31 14:32:01 +0700

    edit result.php , delete profile.php and add result.jpg

--- result.php
+++ result.php
@@ -1,4 +1,17 @@
 <?php
-    include_once 'setup.php';
-    
-    $link = connectDB();
+//    include_once 'setup.php';
+//    $link = connectDB();
+//    
+//    $query = "";
+//    mysql_query($query, $link);
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Title</title>
+    </head>
+    <body>
+        <td><img src="image/result.jpg"></td>   
+    </body>
+</html>
