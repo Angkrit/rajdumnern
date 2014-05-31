afed9936c2a44bc13e8d9fe979e5c419d50af548
kero381 <kero0381@gmail.com>
2014-05-31 12:54:11 +0700

    update add pc_id

--- index.php
+++ index.php
@@ -12,11 +12,17 @@ if ( isset($_POST['button']) AND $_POST['button'] == 'Submit' ) {
 	$pc_accuracy_point = $_POST['skill4'];
 	$pc_comment = $_POST['comment'];
 	
+	$sql = "SELECT pc_id FROM raj_point_comment WHERE ven_id = '$ven_id' ORDER BY pc_id DESC;";
+	$result = mysql_query($sql,$link);
+	$array = mysql_fetch_assoc($result);
+	
+	$pc_id = $array['pc_id']+1;
+	
 	$strSQL = "";
 	$strSQL = "INSERT INTO raj_point_comment ";
-	$strSQL .= "(ven_id,pc_speaking_point,pc_knowledge_point,pc_entertainment_point,pc_accuracy_point,pc_comment )";
+	$strSQL .= "(ven_id,pc_id,pc_speaking_point,pc_knowledge_point,pc_entertainment_point,pc_accuracy_point,pc_comment )";
 	$strSQL .= "VALUES ";
-	$strSQL .= "('$ven_id','$pc_speaking_point','$pc_knowledge_point','$pc_entertainment_point','$pc_accuracy_point','$pc_comment' )";
+	$strSQL .= "('$ven_id','$pc_id','$pc_speaking_point','$pc_knowledge_point','$pc_entertainment_point','$pc_accuracy_point','$pc_comment' )";
 	$objQuery = mysql_query($strSQL,$link);
 
 }
